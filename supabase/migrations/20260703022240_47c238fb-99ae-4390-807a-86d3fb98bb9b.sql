c22fdd8868b2fc309c20b78adb06f065e7e65ea5
HADEESFX <ivanphiri2@gmail.com>
2026-07-11 21:50:46 +0200

    change and add this

--- supabase/migrations/20260703022240_47c238fb-99ae-4390-807a-86d3fb98bb9b.sql
+++ supabase/migrations/20260703022240_47c238fb-99ae-4390-807a-86d3fb98bb9b.sql
@@ -71,6 +71,7 @@ BEGIN
   RETURN NEW;
 END; $$;
 
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
